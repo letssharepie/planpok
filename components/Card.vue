87b7da04c369beafe64165bc36d0857f184d4d01
mdanneil <ddewee92@gmail.com>
2023-06-19 21:23:14 +0200

    add static fibonacci file, add card component with some logic

--- components/Card.vue
+++ components/Card.vue
@@ -1,3 +1,34 @@
 <template>
-    
+  <div>
+    <b-card class="card-main">
+      <b-card-title class="card-title">
+        {{ fibonacciNumber }}
+      </b-card-title>
+      <b-card-footer>
+        <b-button variant="primary">Select</b-button>
+      </b-card-footer>
+    </b-card>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    fibonacciNumber: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.card-main {
+  width: 200px;
+  height: 300px;
+}
+
+.card-title {
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <Board/>
+  <div class="card-container">
+    <Card :fibonacciNumber="number" v-for="number in fibonacci" :key="number" />
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import Card from "~/components/Card.vue";
+import fibonacci from "../static/fibonacci.json";
 
-export default Vue.extend({
-  name: 'IndexPage'
-})
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      fibonacci: fibonacci,
+    };
+  },
+};
 </script>
+
+<style>
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 10px;
+}
+</style>
