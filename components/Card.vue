6ac7f7ea9ec474fc1066cb2267b610976d928a36
mdanneil <ddewee92@gmail.com>
2023-06-20 18:39:06 +0200

    add more styling to Card and Deck components, fix embarassing fibonacci array, add some onclick logic to Cards

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <b-card class="card-main">
+    <b-card @click="openModal" class="card-main" tag="button">
       <b-card-title class="card-title">
         {{ fibonacciNumber }}
       </b-card-title>
-      <b-card-footer class="card-footer" bottom>
-        <b-button variant="primary">Select</b-button>
-      </b-card-footer>
     </b-card>
+
+    <b-modal v-model="showModal" title="Selected Number">
+      <p>You picked number {{ fibonacciNumber }}</p>
+      <template #modal-footer>
+        <b-button variant="primary" @click="closeModal">OK</b-button>
+      </template>
+    </b-modal>
   </div>
 </template>
 
@@ -19,24 +23,68 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
 };
 </script>
 
 <style>
-div {
-  padding: 20px;
-}
-
 .card-main {
   width: 200px;
   height: 300px;
-  padding: 20px;
+  margin: auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+.card-main:hover {
+  background-color: #707070;
 }
 
 .card-title {
   text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  color: #333333;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.b-modal {
+  text-align: center;
+}
+
+.b-modal-header {
+  background-color: #f0f0f0;
+  border-bottom: none;
+}
+
+.b-modal-body {
+  padding: 20px;
+}
+
+.b-modal-footer {
+  background-color: #f0f0f0;
+  border-top: none;
+  display: flex;
+  justify-content: center;
 }
 </style>
--- components/Deck.vue
+++ components/Deck.vue
@@ -23,6 +23,26 @@ export default {
 <style>
 .card-container {
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(5, minmax(200px, 1fr));
+  grid-gap: 20px;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+}
+
+.card-container:last-child {
+  justify-content: flex-start;
+}
+
+@media (max-width: 768px) {
+  .card-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (max-width: 480px) {
+  .card-container {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 </style>
